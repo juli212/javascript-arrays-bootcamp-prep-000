7a0307946d668da04dfb1a1d2d1e87ac326ba5f5
Julia Giraldi <jjgiraldi@gmail.com>
2017-12-18 02:59:52 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,27 @@
+var chocolateBars = [
+  "snickers", "hundred grand", "kitkat", "skittles"  
+]
+
+function addElementToBeginningOfArray(array, element) {
+  const a = [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  
+}
+
+function addElementToEndOfArray(array, element) {
+  
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function descructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift()
+}
