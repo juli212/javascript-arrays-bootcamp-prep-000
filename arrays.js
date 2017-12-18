17c411e0b99fe4a707e39bc1b2df916a3d092479
Julia Giraldi <jjgiraldi@gmail.com>
2017-12-18 03:11:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,7 +24,7 @@ function accessElementInArray(array, index) {
   return array[index]
 }
 
-function descructivelyRemoveElementFromBeginningOfArray(array) {
+function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array.shift()
 }
 
